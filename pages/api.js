5c0caf48f01d8927f8e02a252a53d09ed4e91296
lucasFelixSilveira <lucasdwbfff@gmail.com>
2022-11-19 15:45:32 -0300

    test a fetch

--- pages/api.js
+++ pages/api.js
@@ -1,10 +1,16 @@
 const {FIreray} = require('../src/date.json')
+const fetch = require('node-fetch')
 
 function Home(req, res) {
-    res.status(200).json({
-        FIreray: {
-            version: FIreray.version
+    fetch("https://lucasfelixsilveira.github.io/my_api/", {
+        "method": "GET",
+        "headers": {
+            "user-agent": "vscode-restclient"
         }
+    })
+    .then(res => res.json())
+    .then(res_ => {
+        res.send(200).json(res_)
     })
 }
 
